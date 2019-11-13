7408d18c51e32b2b0d81e65e13d157649e1d97f0
kinming lee <kmleeam@connect.ust.hk>
2019-11-13 23:23:49 +0100

    do some test

--- src/js/test.js
+++ src/js/test.js
@@ -1,5 +1,14 @@
-import { Memorygame } from '../js/memory-game.js'
+/* import { Memorygame } from '../js/memory-game.js'
 
 var newgame = new Memorygame()
 
-newgame.generatenumberphoto()
+newgame.generatenumberphoto() */
+var assert = chai.assert
+
+describe('Array', function () {
+  it('should start empty', function () {
+    var arr = []
+
+    assert.equal(arr.length, 0)
+  })
+})
