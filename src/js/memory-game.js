ea6de09ac46c3929da6c47ee39361bcc0dcfd862
kinming lee <kmleeam@connect.ust.hk>
2019-11-19 16:54:52 +0100

    add template to memory game js file

--- src/js/memory-game.js
+++ src/js/memory-game.js
@@ -1,3 +1,21 @@
+var template = document.createElement('template')
+template.innerHTML = /* HTML */
+`
+<style>
+td{
+    border:6px solid #9b3b00;
+    width:30px;
+    
+}
+
+.removed{
+  visibility: hidden;
+}
+</style>
+<table id = 'gameboard'>
+</table>
+`
+
 class memorygame extends HTMLElement {
 
 }
