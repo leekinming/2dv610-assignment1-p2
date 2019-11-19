0ff0dfb2b6eb271d69164513e27ebe48760770e7
kinming lee <kmleeam@connect.ust.hk>
2019-11-19 16:52:23 +0100

    change the name of describe

--- src/js/testplayer.js
+++ src/js/testplayer.js
@@ -1,20 +1,20 @@
 var assert = chai.assert
 
-describe('constructor', function () {
+describe('player constructor', function () {
   it('it will not use default playername when you have given it', function () {
     var newplayer = new player(123)
     assert.equal(newplayer.getplayername(), '123')
   })
 })
 
-describe('constructor', function () {
+describe('player constructor', function () {
   it('it will use default playername when you have not given it', function () {
     var newplayer = new player()
     assert.equal(newplayer.getplayername(), 'Player')
   })
 })
 
-describe('constructor', function () {
+describe('player constructor', function () {
   it('it can handle different kinds of sysbal', function () {
     var newplayer = new player('!@#$% %$$#?":{}')
     assert.equal(newplayer.getplayername(), '!@#$% %$$#?":{}')
