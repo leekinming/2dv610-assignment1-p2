7b9e8fd2a65762a54a27d22fcbf880ac14eb9465
kinming lee <kmleeam@connect.ust.hk>
2019-11-19 16:49:26 +0100

    GREEN for second test case

--- src/js/testplayer.js
+++ src/js/testplayer.js
@@ -6,3 +6,10 @@ describe('constructor', function () {
     assert.equal(newplayer.getplayername(), '123')
   })
 })
+
+describe('constructor', function () {
+  it('it will use default playername when you have not given it', function () {
+    var newplayer = new player()
+    assert.equal(newplayer.getplayername(), 'Player')
+  })
+})
